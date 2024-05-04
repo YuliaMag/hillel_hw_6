4cfb293c1d632fbef37ed21c8f7f7c8772e3ada7
Yulia Magonya <ymagonia@gmail.com>
2024-05-04 19:15:12 +0300

    qwerty

--- LECT6_HW3.py
+++ LECT6_HW3.py
@@ -1,8 +1,20 @@
+# print(max(open("test.txt", "r"), key=len))
+# ========================================= #
+# dic_t = {}
+# with open("test.txt", "r") as f:
+#     for ln in f:
+#         dic_t[len(ln)] = ln
+# print(dic_t[sorted(dic_t)[-1]])
+# ========================================= #
 
-dic_t = {}
 
+max_ln = " "
+len_max_ln = 0
 with open("test.txt", "r") as f:
     for ln in f:
-        dic_t[len(ln)] = ln
+        if len(ln) >= len_max_ln:
+            len_max_ln = len(ln)
+            max_ln = ln
+    print(max_ln)
 
-print(dic_t[sorted(dic_t)[-1]])
+
